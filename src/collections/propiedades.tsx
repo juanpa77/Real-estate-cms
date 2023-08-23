3e97a97c2f80db5cedbf19330e8e769d8e292b61
Juan Rodriguez <juanpa.box.77@gmail.com>
2023-08-22 21:11:55 -0300

    init project

---
+++ src/collections/propiedades.tsx
@@ -0,0 +1,86 @@
+import { buildCollection, buildProperty } from "firecms"
+
+export interface Property {
+  price: number
+  rooms: number
+  bathrooms: number
+  category: PropertyType[]
+  neighborhood: string
+  departments: string
+  address: string
+  description: string
+  main_image: string;
+}
+
+type PropertyType = 'Casa' | 'Apartamento'
+
+export const realEstateCollection = buildCollection<Property>({
+  name: "Propiedades",
+  path: "Propiedades",
+  group: "Inmobiliaria",
+  properties: {
+    price: {
+      name: "Precio",
+      validation: { required: true },
+      dataType: 'number'
+    },
+    rooms: {
+      name: 'cantidad de habitaciones',
+      validation: { required: true },
+      dataType: 'number'
+    },
+    bathrooms: {
+      name: 'Nro de baños',
+      validation: { required: true },
+      dataType: 'number'
+    },
+    category: {
+      name: 'category',
+      validation: { required: true },
+      dataType: 'array',
+      of: {
+        dataType: 'string',
+        enumValues: {
+          casa: 'Casa',
+          apartamento: 'Apartamento',
+          localComercial: 'Local Comercial',
+          deposito: 'Deposito',
+        }
+      }
+    },
+    neighborhood: {
+      name: 'barrio',
+      validation: { required: true },
+      dataType: 'string'
+    },
+    departments: {
+      name: 'departamento',
+      validation: { required: true },
+      dataType: 'string'
+    },
+    address: {
+      name: 'dirección',
+      validation: { required: true },
+      dataType: 'string'
+    },
+    description: {
+      name: 'descripción',
+      validation: { required: true },
+      dataType: 'string'
+    },
+    main_image: buildProperty({ // The `buildProperty` method is a utility function used for type checking
+      name: "Image",
+      dataType: "string",
+      storage: {
+        storagePath: "images",
+        acceptedFiles: ["image/*"]
+      }
+    }),
+  },
+  permissions: () => ({
+    read: true,
+    edit: true,
+    create: true,
+    delete: true
+  }),
+})
---
+++ src/firebase-config.ts
@@ -0,0 +1,10 @@
+// TODO: Replace with your config
+export const firebaseConfig = {
+    apiKey: "AIzaSyCSOYPUESpN4QS598jzYGT6kaSzsn7IuyM",
+    authDomain: "real-estate-cms-9b6be.firebaseapp.com",
+    projectId: "real-estate-cms-9b6be",
+    storageBucket: "real-estate-cms-9b6be.appspot.com",
+    messagingSenderId: "91132851986",
+    appId: "1:91132851986:web:12184e5d92bc134d3225cb",
+    measurementId: "G-H9B9RJMLEY"
+};
